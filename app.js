141c180063dabd2cd6cc8ac8757328f681ed14dd
diosleonor <tccoolovl@gmail.com>
2021-11-09 12:09:52 +0800

    add handlebars

--- app.js
+++ app.js
@@ -14,10 +14,17 @@ const db = mongoose.connection
 db.on('error', () => console.log('Mongoose error.'))
 db.once('open', () => console.log('Mongoose connected.'))
 
+// handlebars start
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname:'.hbs'}))
+app.set('view engine', 'hbs')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 //route setting
+app.get('/', (req,res) => {
+	res.render('index')
+})
 
 app.listen(PORT, () => {
 	console.log(`App is running on http://localhost:${PORT}`)})
